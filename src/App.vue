1e4132aca965fbe275ec57e115b31e418b54779c
unknown <ballot.marius@gmail.com>
2021-04-05 19:58:02 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <InConstruction />
+    <ThreeScene />
+</template>
+
+<script>
+import ThreeScene from "./components/ThreeScene.vue";
+import InConstruction from "./components/InConstruction.vue";
+
+export default {
+    name: "App",
+    components: {
+        ThreeScene,
+        InConstruction,
+    },
+};
+</script>
+
+<style lang="stylus">
+@import url('https://fonts.googleapis.com/css?family=Rubik:400,500,700,900&display=swap');
+
+@font-face {
+    font-family: Butler;
+    font-style: bold;
+    src: url('assets/fonts/Butler-Bold.woff');
+}
+
+#app {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+}
+
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Rubik', sans-serif;
+}
+</style>
---
+++ src/components/InConstruction.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="inConstruction">
+        <h1>Marius Ballot</h1>
+        <div class="line"></div>
+        <h3>Engineering student by day, weird pixel maker by night</h3>
+        <p>Minor modifications and fixes in progress</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "inConstruction",
+    props: {
+        msg: String,
+    },
+    mounted() {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+.inConstruction {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    justify-content: center;
+    align-items: center;
+    background: white;
+
+    h1 {
+        font-family: 'Bulter';
+        weight: bold;
+        font-size: 4em;
+    }
+
+    .line {
+        height: 2px;
+        width: 300px;
+        background: linear-gradient(#AAAAFF, #FFAAFF);
+        margin-top: 10px;
+        margin-bottom: 20px;
+    }
+
+    p {
+        margin-top: 5px;
+    }
+}
+</style>
